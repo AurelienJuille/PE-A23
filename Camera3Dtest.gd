0e26aeea3cdb36edad3bfa364240555431a476bb
SwanBones <xuan.boone@gmail.com>
2023-10-26 18:48:24 +0200

    update

--- Camera3Dtest.gd
+++ Camera3Dtest.gd
@@ -2,16 +2,22 @@ extends Camera3D
 
 var timer = .0
 var target = Vector3.ZERO
-var offset = Vector3.ZERO
+var cameraTransition = Vector3.ZERO
+@export var yPosition : float
+@export var yRotation : float
+@export var height : float
+@export var distanceFromPlayer : float
 
 func _physics_process(delta):
+	var cameraPositionOffset = Vector3(0,height,distanceFromPlayer)
 	var player_pos = get_parent().get_node("PLAYER").global_position
-	offset = lerp(offset, target, .05)
-	position = Vector3(player_pos.x, 0, player_pos.z) + offset
-	look_at(Vector3(player_pos.x, 0.1, player_pos.z))
+	cameraTransition = lerp(cameraTransition, target, .3)
+	position = Vector3(player_pos.x, yPosition, player_pos.z) + cameraTransition + cameraPositionOffset
+	look_at(Vector3(player_pos.x, yRotation, player_pos.z))	
 
 	timer -= delta
-	if timer <= .0:
-		target = Vector3(randf_range(-.2, .2), randf_range(.3, .5), .8)
-		timer = 3.0
+	
+#	if timer <= .0:
+#		target = Vector3(randf_range(-0, 0), randf_range(0, 0), .8)
+#		timer = 3.0
 	
